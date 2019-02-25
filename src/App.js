f6a232142231194348985b2bce46fdb74638df0d
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-25 14:53:27 -0500

    Added components to Routes, now rendering correctly

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route path='/' />
-    <Route path='/about' />
-    <Route path='/contact' />
+    <Route path='/' exact component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/contact' component={Contact} />
   </div>
 );
 
